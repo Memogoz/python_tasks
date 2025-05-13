8109340997f5c4ab3d2395bd5dde10fb5a15285e
Memogoz <guillermoglez583@gmail.com>
2025-05-13 09:24:07 -0600

    feat: add script 1 to get file extensions from file names

---
+++ Task 1/1_FileExtension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Script to get the file extension from a file
+import sys
+
+# Check number of arguments
+if len(sys.argv) != 2:
+    print("Usage: ./1_FileExtension.py <filename>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+# Check if filename is valid
+if "\c.\c" not in filename :
+    raise ValueError("Filename must contain at least one dot (.)")
+
+# Get the file extension
+file_extention = filename.split(".")[-1]
+
+print(f"File extension: .{file_extention}")
