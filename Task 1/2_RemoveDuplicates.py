c973849341a23028ecb26ae78d9b564947e7b1b6
Memogoz <guillermoglez583@gmail.com>
2025-05-13 17:31:59 -0600

    fix: modify list and tuple names to fix error related to reserved words

--- Task 1/2_RemoveDuplicates.py
+++ Task 1/2_RemoveDuplicates.py
@@ -11,19 +11,19 @@ if len(sys.argv) != 2:
 
 # Convert argument to list
 try:
-    list = ast.literal_eval(sys.argv[1]) 
-    # Check if its a list of integers
-    if not isinstance(list, list) or not all(isinstance(i, int) for i in list):
+    input_list = ast.literal_eval(sys.argv[1]) 
+    # Check if it's a list of integers
+    if not isinstance(input_list, list) or not all(isinstance(i, int) for i in input_list):
         raise ValueError
 except:
     print('Invalid list format. Format should be "[<int>, <int>, <int>, ...]"')
     sys.exit(1)
 
 # Remove duplicates and create tuple
-tuple = tuple(set(list))
+unique_tuple = tuple(set(input_list))
 
 # Print results
-print(f"List:  {list}")  
-print(f"Tuple: {tuple}")
-print(f"Min: {min(tuple)}")
-print(f"Max: {max(tuple)}")
+print(f"List:  {input_list}")  
+print(f"Tuple: {unique_tuple}")
+print(f"Min: {min(unique_tuple)}")
+print(f"Max: {max(unique_tuple)}")
